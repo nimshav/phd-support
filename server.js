cbff99e98f132928095f75a75a27dcea7c8172bb
nimshav <88726093+nimshav@users.noreply.github.com>
2025-01-05 19:21:04 +0200

    Fix start script and server port

--- server.js
+++ server.js
@@ -2,19 +2,17 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-
-// Middleware to serve static files from the React app's build folder
-app.use(express.static(path.join(__dirname, 'build')));
-
-// Catch-all route to serve the React app for all requests
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-// Define the port to run the server
 const PORT = process.env.PORT || 3000;
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// Serve static files from React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Catch-all for requests
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
